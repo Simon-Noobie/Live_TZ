73ab18ce6b92bfedd48f5e17db993626c1e6bae3
Simon-Noobie <Simon-noobie@users.noreply.github.com>
2025-07-15 09:23:59 +0530

    Add user registration and login templates, update DB config
    Added login and registration HTML templates for user authentication. Introduced a UserProfile model linked to Django's User model. Updated database settings to use MySQL instead of SQLite. Added several .DS_Store files (should be ignored in .gitignore).

--- Live_TZ/settings.py
+++ Live_TZ/settings.py
@@ -79,8 +79,8 @@ ASGI_APPLICATION = 'Live_TZ.asgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': roo',
     }
 }
 
